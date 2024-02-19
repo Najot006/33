219f03d86f1b061e8ced0c6fea556b8f3152d201
Your Name <you@example.com>
2024-02-19 12:28:06 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center  p-24">
+      <h1>Home page </h1>
+    </main>
+  );
+}
---
+++ app/ui/dashboard/sidebar/menuLink/menuLink.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React, { ReactNode } from 'react'
+import styles from "./menuLink.module.css"
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+interface SubItem {
+    title: string;
+    path: string;
+    icon: ReactNode;
+}
+
+export default function MenuLink({menu}:Readonly<{
+    menu: SubItem;
+}>) {
+    const pathname = usePathname()
+  return (
+    <Link href={menu.path} className={`${styles.container} ${pathname === menu.path && styles.active}`}>
+      {menu.icon}
+      {menu.title}
+    </Link>
+  )
+}
